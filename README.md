2a8a3dc36df651932d69b6a17268798fca1a0347
jonathantcrawford <jon_t_crawford@protonmail.com>
2021-01-05 20:35:57 -0500

    chore: add rename support

--- README.md
+++ README.md
@@ -1,7 +1,25 @@
 # pre-reqs
 cmake 3.15.5
 
-# getting started
+# initial repo setup
+1. clone repo using ssh or https
+- ssh: `git clone --recurse-submodules git@github.com:jonathantcrawford/juce-sandbox.git`
+- https: `git clone --recurse-submodules https://github.com/jonathantcrawford/juce-sandbox.git`
+
+2. set directory to project root
+- `cd juce-sandbox`
+
+3. set permisions for bash scripts
+- `chmod u+x rename.sh`
+
+4. rename project files
+- `./rename.sh`
+- `name for repo, kebab case (juce-sandbox): juce-sandbox`
+- `enter name of app, pascal case (JuceSandbox): JuceSandbox`
+- `enter name of app, all caps + snake case (JUCE_SANDBOX): JUCE_SANDBOX`
+
+
+# using repo
 1. clone repo using ssh or https
 - ssh: `git clone --recurse-submodules git@github.com:jonathantcrawford/juce-sandbox.git`
 - https: `git clone --recurse-submodules https://github.com/jonathantcrawford/juce-sandbox.git`
---
+++ rename.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+oldreponame="juce-sandbox"
+read -p "name for repo, kebab case (juce-sandbox): " newreponame
+
+oldappname="JuceSandbox"
+read -p "enter name of app, pascal case (JuceSandbox): " newappname
+
+oldappcapsname="JUCE_SANDBOX"
+read -p "enter name of app, all caps + snake case (JUCE_SANDBOX): " newappcapsname
+
+
+find . -name 'README.md' -print0 | xargs -0 sed -i "" "s/$oldreponame/$newreponame/g"
+find . -name 'README.md' -print0 | xargs -0 sed -i "" "s/$oldappname/$newappname/g"
+find . -name 'README.md' -print0 | xargs -0 sed -i "" "s/$oldappcapsname/$newappcapsname/g"
+
+find . -name 'Main.cpp' -print0 | xargs -0 sed -i "" "s/$oldappname/$newappname/g"
+
+find . -name 'CMakeLists.txt' -print0 | xargs -0 sed -i "" "s/$oldappcapsname/$newappcapsname/g"
+find . -name 'CMakeLists.txt' -print0 | xargs -0 sed -i "" "s/$oldappname/$newappname/g"
+
+find . -name 'rename.sh' -print0 | xargs -0 sed -i "" "s/$oldreponame/$newreponame/g"
+find . -name 'rename.sh' -print0 | xargs -0 sed -i "" "s/$oldappname/$newappname/g"
+find . -name 'rename.sh' -print0 | xargs -0 sed -i "" "s/$oldappcapsname/$newappcapsname/g"
+
+mv ../{$oldreponame,$newreponame}
