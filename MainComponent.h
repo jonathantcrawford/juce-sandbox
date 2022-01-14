c82705c6e865dbea4f0c22209488e82f72d5e3b6
jonathantcrawford <jon_t_crawford@protonmail.com>
2022-01-14 12:04:57 -0600

    consolidated maincomponent to header

--- MainComponent.h
+++ MainComponent.h
@@ -16,11 +16,26 @@ class MainComponent   : public juce::Component
 {
 public:
     //==============================================================================
-    MainComponent();
-
+    MainComponent()
+    {
+        DBG( "MainComponent Constructor" );
+        setSize (600, 400);
+    }
     //==============================================================================
-    void paint (juce::Graphics&) override;
-    void resized() override;
+    void paint (juce::Graphics& g ) override
+    {
+        // (Our component is opaque, so we must completely fill the background with a solid colour)
+        g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+
+        g.setFont (juce::Font (16.0f));
+        g.setColour (juce::Colours::white);
+        g.drawText ("Hello World!", getLocalBounds(), juce::Justification::centred, true);
+    }
+
+    void resized() override
+    {
+
+    }
 
 private:
     //==============================================================================
